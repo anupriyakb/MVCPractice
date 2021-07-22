fff40ab813caffc4fcfe454e8a70ad8bbe9cde6c
Anusha <anusha14pr@gmail.com>
2021-07-22 23:10:21 +0530

    JWT Token generation

---
+++ ShoppingCart/Models/JWTConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public class JWTConstants
+    {
+        public const string Issuer = "MVS";
+        public const string Key = "Super secret key for JWT Token";
+    }
+}
